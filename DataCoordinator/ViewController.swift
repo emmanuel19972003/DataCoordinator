55517e85b619868445243dcd9831c01db0ae517a
Emma <emmauel1997@mail.com>
2024-07-05 17:18:41 -0500

    Initial Commit

---
+++ DataCoordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataCoordinator
+//
+//  Created by Emmanuel Zambrano on 5/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
